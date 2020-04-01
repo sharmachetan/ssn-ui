579b55f3a73231725ad3768b76ef7c67a729356c
chetan <sharmachetan6@gmail.com>
2020-04-01 18:05:55 +0100

    separated top bar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
+
   <div class="home">
+      <TopBar/>
+
     <img alt="Vue logo" src="../assets/UlLogo.png" />
     <HelloWorld msg="Welcome to Research Project Portal" />
     <LoginAs/>
@@ -12,12 +15,15 @@
 import HelloWorld from "@/components/HelloWorld.vue";
 // import loginForm from "@/components/LoginForm.vue";
 import LoginAs from "@/components/LoginAs.vue";
+import TopBar from "@/components/TopBar.vue";
+
 
 export default {
   name: "Home",
   components: {
     HelloWorld,
-    LoginAs
+    LoginAs,
+    TopBar
     // loginForm
 
   }
