a9aa1306cda56aee29bb4190f93ba67cc9807c0f
chetan <sharmachetan6@gmail.com>
2020-04-04 17:38:32 +0100

    add proposal meta data table

--- src/components/document/AuthorDetail.vue
+++ src/components/document/AuthorDetail.vue
@@ -1,3 +1,60 @@
 <template>
-    <div><h5>Author Chetan </h5></div>
+    <div id="AuthorDiv">
+ 
+<table cellspacing="0" cellpadding="0">
+    <tr>
+        <td><h5>Author :</h5></td>
+        <td>Chetan Sharma</td>
+    </tr>
+   
+    <tr> 
+        <td><h5>Department :</h5></td>
+        <td>Information And Network Security</td>
+    </tr>
+    <tr>
+<td><h5>Status :</h5></td>
+<td>In Progress</td>
+
+    </tr>
+      <tr>
+<td><h5>Supervised By :</h5></td>
+<td>Dr. Reiner Dojen</td>
+
+    </tr>
+</table>
+
+    </div>
 </template>
+
+
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+table tr td {
+    padding: 0;
+    border-spacing: 0;
+    text-align: right;
+    border-bottom: 1px solid #ddd;
+    height: 0;
+    font-size: 10px;
+    padding-left: 20px;
+    
+    
+}
+table tr td h5{
+    height:inherit;
+}
+
+table tr {
+    
+}
+
+
+
+</style>
--- src/components/document/TimeStamp.vue
+++ src/components/document/TimeStamp.vue
@@ -1,3 +1,28 @@
 <template>
-    <div><h6>Last Updated :</h6></div>
+    <div id="UpdateInfo">
+        <div id="lUpdate">
+        <h6>Last Updated : </h6>
+        </div>
+        </div>
 </template>
+
+<script>
+export default {
+    
+}
+
+</script>
+<style scoped>
+#UpdateInfo {
+    width: 100%;
+    padding: 0;
+float: right;
+right: 50px;
+}
+#lUpdate {
+    padding: 0;
+    margin: 0;
+    float:right;
+}
+</style>>
+
